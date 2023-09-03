ceaf342798b6691888f4f2ac674b2451e34bec1d
Eddarif Badr <skyzoxcodes@gmail.com>
2023-09-03 02:49:06 +0100

    optimizing code

--- source/System/json_parse.js
+++ source/System/json_parse.js
@@ -1,19 +1,18 @@
 function ParseToJson(object, vars) {
     var _object_string = new String(object).toString()
     var result = {}
-    if (_object_string.startsWith('!') && _object_string.endsWith('!')) {
-        const args = _object_string.replace(/[!]/g, '')
+    if (_object_string.startsWith('{') && _object_string.endsWith('}')) {
+        const args = _object_string.replace(/[{}]/g, '')
         const object_args = args.split(":").join(" ").split(",")
         object_args.forEach((section) => {
-            const contents = section.trim().split(" ");
-            const object_name = contents[0]
-            if ( contents.slice(1).join(" ").trim().startsWith('"') &&  contents.slice(1).join(" ").trim().endsWith('"')) {
-                result[object_name] = contents.slice(1).join(" ").trim().replace(/[""]/g, '')
+            const contents = section.trim().split(" ").slice(1).join(" ").trim();
+            const object_name = section.trim().split(" ")[0]
+            if ( contents.startsWith('"') &&  contents.endsWith('"')) {
+                result[object_name] = contents.replace(/[""]/g, '')
             } else {
-                const momeryVars = vars.find(_vars => _vars.name == contents.slice(1).join(" ").trim())
+                const momeryVars = vars.find(_vars => _vars.name == contents)
                 result[object_name] = momeryVars.content
             }
-            
         })
         return result
     }
